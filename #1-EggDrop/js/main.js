7120b893e959d18add3fc410dd2802d26ef409ac
Yoni <yoni9258@naver.com>
2021-06-27 22:52:34 +0900

    header작업완료

---
+++ #1-EggDrop/js/main.js
@@ -0,0 +1,36 @@
+'use strict'
+
+const gnb = document.querySelectorAll('.gnb li')
+const lnb = document.querySelector('.lnb')
+const lnb__list = document.querySelectorAll('.lnb__list')
+
+// 메뉴 마우스 호버시 slideDown
+gnb.forEach((gnb) => {
+    lnb__list.forEach((lnb__list) => {
+        gnb.addEventListener("mouseover", () => {
+            gsap.to(lnb, 0.5, {
+                opacity: 1,
+                display: 'flex',
+                top: 110,
+
+            })
+            gsap.to(lnb__list, 0.5, {
+                opacity: 1,
+                top: 0
+            })
+        })
+
+    })
+    lnb.addEventListener("mouseleave", () => {
+        gsap.to(lnb, 0.2, {
+            opacity: 0,
+            display: 'none',
+            top: -210,
+           
+
+        })
+        gsap.to(lnb__list, 0.1, {
+            opacity: 0,
+        })
+    })
+})
