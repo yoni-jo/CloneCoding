cc1476924ec3cf0626146084d46d0d82401f6c47
Yoni <yoni9258@naver.com>
2021-06-27 23:15:50 +0900

    header menu 마우스이벤트 수정

--- #1-EggDrop/js/main.js
+++ #1-EggDrop/js/main.js
@@ -7,30 +7,31 @@ const lnb__list = document.querySelectorAll('.lnb__list')
 // 메뉴 마우스 호버시 slideDown
 gnb.forEach((gnb) => {
     lnb__list.forEach((lnb__list) => {
-        gnb.addEventListener("mouseover", () => {
+        gnb.addEventListener("mouseenter", () => {
             gsap.to(lnb, 0.5, {
                 opacity: 1,
                 display: 'flex',
-                top: 110,
-
+                top: 110
             })
-            gsap.to(lnb__list, 0.5, {
+            gsap.to(lnb__list, .6, {
+                delay: .5,
                 opacity: 1,
-                top: 0
+                top: 0,
+                display:'flex'
             })
         })
 
     })
     lnb.addEventListener("mouseleave", () => {
-        gsap.to(lnb, 0.2, {
+        gsap.to(lnb, 0.1, {
             opacity: 0,
             display: 'none',
-            top: -210,
-           
+            top: -210
 
         })
         gsap.to(lnb__list, 0.1, {
             opacity: 0,
+            display:'none'
         })
     })
 })
