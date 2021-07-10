8e06fbd7d1c529d0f46e16a0593e2eb8b048fd6b
Yoni <yoni9258@naver.com>
2021-07-11 01:33:25 +0900

    이름 입력후 h1태그에 환영메시지 뜨게 하고 form태그는 사라지게 하기

--- #2-CromeApp/js/login.js
+++ #2-CromeApp/js/login.js
@@ -1,3 +1,16 @@
 'use strict'
+const LoginForm = document.querySelector('.login-form');
+const login_btn = LoginForm.querySelector('.login-btn');
+const text_input = LoginForm.querySelector('input:first-child')
+const welcome = document.querySelector('.welcome')
 
 
+function submit(event) {
+    event.preventDefault();
+    const username = text_input.value
+    welcome.classList.remove('hide')
+    LoginForm.classList.add('hide')
+    welcome.innerText = `Welcome !${username}~!`
+}
+
+login_btn.addEventListener('click', submit)
