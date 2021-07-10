9284dd5fac1c8f4d1e30791e3875e9eb91eb7be2
Yoni <yoni9258@naver.com>
2021-07-11 01:42:04 +0900

    localstore에 이름저장 후 유무에 따라 form이 보이고 안보이게 수정

--- #2-CromeApp/js/login.js
+++ #2-CromeApp/js/login.js
@@ -4,13 +4,25 @@ const login_btn = LoginForm.querySelector('.login-btn');
 const text_input = LoginForm.querySelector('input:first-child')
 const welcome = document.querySelector('.welcome')
 
-
+//1. 이름 입력후h1태그에 환영인사 뜨게하기
 function submit(event) {
     event.preventDefault();
     const username = text_input.value
     welcome.classList.remove('hide')
     LoginForm.classList.add('hide')
     welcome.innerText = `Welcome !${username}~!`
+    localStorage.setItem('username', username)
 }
 
-login_btn.addEventListener('click', submit)
+
+
+//2.이름이 등록되어있는지 유무 확인하기 
+
+const storage_name = localStorage.getItem('username')
+if (storage_name === null) {
+    LoginForm.classList.remove('hide')
+    login_btn.addEventListener('click', submit)
+}else{
+    welcome.innerText = `Welcome !${storage_name}~!`
+    welcome.classList.remove('hide')
+}
