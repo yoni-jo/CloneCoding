ecdb0873a7065efab63e23819e55b116972a15e4
Yoni <yoni9258@naver.com>
2021-07-11 01:54:17 +0900

    실시간으로 현재시간 부르기위해
    setinterval설정

--- #2-CromeApp/js/clock.js
+++ #2-CromeApp/js/clock.js
@@ -1,10 +1,18 @@
 "use strict"
 
 const clock = document.querySelector('.clock');
-const time = new Date()
-const hours =time.getHours()
-const minutes = time.getMinutes()
-const seconds = time.getSeconds()
+
 
 //실시간으로 현재 시간 알려주기
-clock.innerHTML = `${hours}:${minutes}:${seconds}`
+function CurrentTime(){
+    const time = new Date()
+    const hours =time.getHours()
+    const minutes = time.getMinutes()
+    const seconds = time.getSeconds()
+
+    
+    clock.innerHTML = `${hours}:${minutes}:${seconds}`
+
+}
+CurrentTime()
+setInterval(CurrentTime,1000)
