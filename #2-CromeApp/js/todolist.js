89a4b0b8a9523026f21ca52ccc95a8e766d7a667
Yoni <yoni9258@naver.com>
2021-07-16 18:02:59 +0900

    삭제 함수 수정

--- #2-CromeApp/js/todolist.js
+++ #2-CromeApp/js/todolist.js
@@ -8,24 +8,27 @@ let toDos = []
 
 const TODOS_KEY = 'toDos'
 // 저장
-function saveTodo(toDos) {
+function saveTodo() {
     localStorage.setItem(TODOS_KEY, JSON.stringify(toDos))
 }
 // 삭제
 function deleteTodo(event) {
     const li = event.target.parentElement
     li.remove()
+    toDos=toDos.filter((toDo)=>toDo.id !==parseInt(li.id))
+    saveTodo()
 }
 
 // todolist에 추가
 function createTodo(newTodo) {
     const li = document.createElement('li')
+    li.id = newTodo.id
     const span = document.createElement('span')
     const button = document.createElement('button')
     todoList.appendChild(li)
     li.appendChild(span)
     li.appendChild(button)
-    span.innerText = newTodo
+    span.innerText = newTodo.text
     button.innerText = 'del'
     button.addEventListener('click', deleteTodo)
 }
@@ -34,9 +37,13 @@ function createTodo(newTodo) {
 function handleToDoSubmit(event) {
     event.preventDefault();
     const newTodo = toDoInput.value
+    const newTodoObj = {
+        text:newTodo,
+        id:Date.now()
+    }
     toDoInput.value = ""
-    toDos.push(newTodo)
-    createTodo(newTodo)
+    toDos.push(newTodoObj)
+    createTodo(newTodoObj)
     saveTodo(toDos)
 }
 
@@ -48,10 +55,7 @@ if (saveTodos) {
     //locaclstrorage안에 todos가 있을경우
     const parseTodos = JSON.parse(saveTodos)
     toDos=parseTodos
-    parseTodos.forEach((item) => {
-        createTodo(item)
-      
-    })
+    parseTodos.forEach(createTodo)
 } else {
 
 }
